986fa2dcfc301a657a83da5343bda7ab67ebd670
IvanTanev <vankata.t@gmail.com>
2013-10-26 18:18:16 +0300

    More deps auto install

--- install-deps.sh
+++ install-deps.sh
@@ -1,23 +1,27 @@
 # up to you (me) if you want to run this as a file or copy paste at your leisure
 
-if which apt-get > /dev/null; then
+if hash apt-get 2>/dev/null; then
    gimme="sudo apt-get install -y"
-elif which yum > /dev/null; then
+elif hash yum > /dev/null; then
    gimme="sudo yum install -y"
 fi
 
 $gimme curl
+$gimme ack-grep
 
 # https://github.com/jamiew/git-friendly
 # the `push` command which copies the github compare URL to my clipboard is heaven
-bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
+#bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
 # https://rvm.io
 # rvm for the rubiess
-curl -L https://get.rvm.io | bash -s stable --ruby
+if ! hash rvm; then
+    curl -L https://get.rvm.io | bash -s stable --ruby
+fi
 
 # https://github.com/rupa/z
 # z, oh how i love you
+mkdir -p ~/code
 cd ~/code
 git clone https://github.com/rupa/z.git
 chmod +x ~/code/z/z.sh
@@ -25,8 +29,11 @@ chmod +x ~/code/z/z.sh
 
 # z binary is already referenced from .bash_profile
 
-$gimme python python-setuptools
-
 # for the c alias (syntax highlighted cat)
+$gimme python python-setuptools
 sudo easy_install Pygments
 
+# setup pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
