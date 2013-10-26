f960628a5fcf4e7c1cd1f7f91aacc91df49ad066
IvanTanev <vankata.t@gmail.com>
2013-10-26 20:07:18 +0300

    Make the install-deps script idempotent

--- install-deps.sh
+++ install-deps.sh
@@ -1,4 +1,9 @@
 # up to you (me) if you want to run this as a file or copy paste at your leisure
+set -e
+
+git submodule init
+git submodule update
+
 
 if hash apt-get 2>/dev/null; then
    gimme="sudo apt-get install -y"
@@ -8,6 +13,13 @@ fi
 
 $gimme curl
 $gimme ack-grep
+
+# required stuff for Commant-T vim
+if [[ `uname -a` == *Ubuntu* ]]; then
+    $gimme vim-nox ruby ruby-dev
+    cd .vim/bundle/Command-T/
+    /usr/bin/rake1.9.1 make
+fi
 
 # https://github.com/jamiew/git-friendly
 # the `push` command which copies the github compare URL to my clipboard is heaven
@@ -21,23 +33,28 @@ fi
 
 # https://github.com/rupa/z
 # z, oh how i love you
-mkdir -p ~/code
-cd ~/code
-git clone https://github.com/rupa/z.git
-chmod +x ~/code/z/z.sh
-# also consider moving over your current .z file if possible. it's painful to rebuild :)
-# z binary is already referenced from .bash_profile
+if [ ! -d ~/code/z ]; then
+    mkdir -p ~/code
+    cd ~/code
+    git clone https://github.com/rupa/z.git
+    chmod +x ~/code/z/z.sh
+    # also consider moving over your current .z file if possible. it's painful to rebuild :)
+    # z binary is already referenced from .bash_profile
+fi
 
-mkdir -p ~/code/solarize
-cd ~/code/solarize
+# setup solarized
+if [ ! -d ~/code/solarize ]; then
+    mkdir -p ~/code/solarize
+    cd ~/code/solarize
 
-wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
-mv dircolors.ansi-dark ~/.dircolors
-eval `dircolors ~/.dircolors`
+    wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
+    mv dircolors.ansi-dark ~/.dircolors
+    eval `dircolors ~/.dircolors`
 
-git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
-cd gnome-terminal-colors-solarized
-./set_dark.sh
+    git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
+    cd gnome-terminal-colors-solarized
+    ./set_dark.sh
+fi
 
 # for the c alias (syntax highlighted cat)
 $gimme python python-setuptools
