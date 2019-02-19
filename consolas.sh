578892f2e85037c02e6139bf847577ea2a4ee63b
Ivan Tanev <vankata.t@gmail.com>
2019-02-19 02:36:59 -0800

    Fix consolas could not be installed

--- consolas.sh
+++ consolas.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Based on http://www.rushis.com/2013/03/consolas-font-on-ubuntu/
 
-set -e
+set -eu -o pipefail
 set -x
+
 sudo apt-get install font-manager
 sudo apt-get install cabextract
 mkdir -p /tmp/consolas
 cd /tmp/consolas
-wget http://download.microsoft.com/download/E/6/7/E675FFFC-2A6D-4AB0-B3EB-27C9F8C8F696/PowerPointViewer.exe
+if [ ! -f /tmp/consolas/PowerPointViewer.exe ]; then
+    wget https://sourceforge.net/projects/mscorefonts2/files/cabs/PowerPointViewer.exe/download -O PowerPointViewer.exe
+fi
 cabextract -L -F ppviewer.cab PowerPointViewer.exe
 cabextract ppviewer.cab
+xdg-open /tmp/consolas
